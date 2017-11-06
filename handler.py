2441ba6919eff1defd13e2cdd03a3043f3636e78
Philipp Bliznuk <philipp.bliznuk@cshisw.com>
2017-11-06 13:18:08 +0200

    Fix find_correlations and remove *.pyc files

--- handler.py
+++ handler.py
@@ -8,10 +8,15 @@ import json
 import numpy as np
 import boto3
 
+
 MIN_CORRELATION = 0.7
 
+
 def get_values(data, key, cast_to=int):
-    return [cast_to(item[key]['N']) for item in data]
+    return [
+        cast_to(item[key].get('N', item[key].get('S'))) for item in data if key in item
+    ]
+
 
 def get_table_permutations(evernt, content):
     client = boto3.client('dynamodb')
@@ -42,47 +47,54 @@ def find_correlations(event, content):
     item_counts = []
     response_data = OrderedDict({})
     for table in [data_table, sensor_table]:
-        response = client.scan(
-            TableName=table,
-            FilterExpression='timestamp_sort >= :start',
-            ExpressionAttributeValues={':start': {'N': str(start_timestamp)}}
+        response = client.query(
+            TableName='corr_data',
+            KeyConditionExpression='type_of_data = :ToD AND time_added >= :start',
+            ScanIndexForward=True,
+            Limit=1000,
+            ExpressionAttributeValues={
+                ':ToD': {'S': table},
+                ':start': {'N': str(start_timestamp)}
+            }
         )
         count = response['Count']
+        items = response['Items']
+        if 'LastEvaluatedKey' in response:
+            count -= 1
+            items = items[1:]
+
         if not count:
             # No reason to compare if one table is empty
             return response_data
 
         item_counts.append(count)
-        response_data[table] = response['Items']
+        response_data[table] = items
 
     # slice data to have same amount of items
     timestamps = set()
     min_item_count = min(item_counts)
     for table, data in response_data.iteritems():
         response_data[table] = data[:min_item_count]
-        timestamps.update(get_values(response_data[table], 'timestamp'))
+        timestamps.update(get_values(response_data[table], 'time_added'))
 
+    # print response_data['sound']
     # compute interpolation and correlation
     interpolations = {}
     linspaced_timestamps = np.linspace(
         min(timestamps), max(timestamps), num=24 # hours in day
     )
     for table, data in response_data.iteritems():
-        value_key = 'rate'
-        if table == sensor_table:
-            value_key = 'value'
-
         interpolations[table] = np.interp(
             linspaced_timestamps,
-            get_values(data, 'timestamp'),
-            get_values(data, value_key, cast_to=float)
+            get_values(data, 'time_added'),
+            get_values(data, 'value', cast_to=float)
         )
 
     correlation = np.corrcoef(
         interpolations[data_table], interpolations[sensor_table]
     )[0, 1]
 
-    if np.isnan(correlation) or correlation <= MIN_CORRELATION:
+    if np.isnan(correlation) or correlation < MIN_CORRELATION:
         return {}
 
     response_data['correlation'] = correlation
